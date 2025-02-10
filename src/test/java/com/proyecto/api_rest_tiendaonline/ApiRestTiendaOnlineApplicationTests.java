b95b31a1c5bb595930883ae2cbd243563f009cf3
O5CVR <oscarcasillasrodriguez@gmail.com>
2025-02-10 10:49:44 +0100

    Comienzo del proyecto

---
+++ src/test/java/com/proyecto/api_rest_tiendaonline/ApiRestTiendaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.api_rest_tiendaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestTiendaOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
