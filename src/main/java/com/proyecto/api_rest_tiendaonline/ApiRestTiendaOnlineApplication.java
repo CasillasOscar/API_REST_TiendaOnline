bdeeb033ae3e38fbdcf788a770962e2fdc81731c
O5CVR <oscarcasillasrodriguez@gmail.com>
2025-02-16 13:09:21 +0100

    EnableCachin en Apliccation

--- src/main/java/com/proyecto/api_rest_tiendaonline/ApiRestTiendaOnlineApplication.java
+++ src/main/java/com/proyecto/api_rest_tiendaonline/ApiRestTiendaOnlineApplication.java
@@ -2,8 +2,10 @@ package com.proyecto.api_rest_tiendaonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ApiRestTiendaOnlineApplication {
 
     public static void main(String[] args) {
